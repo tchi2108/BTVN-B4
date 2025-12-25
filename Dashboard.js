a0be27acaf357256ae8ff496942b639394e4b574
tchi2108 <dothuychi2006@gmail.com>
2025-12-25 19:08:37 +0700

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,18 +1,21 @@
 import { Routes, Route } from "react-router-dom";
+import { Box, Container } from "@mui/material";
 import Header from "../components/Header";
 import Products from "./Products";
 import Users from "./Users";
 
 const Dashboard = () => {
   return (
-    <div>
+    <Box>
       <Header />
 
-      <Routes>
-        <Route path="products" element={<Products />} />
-        <Route path="users" element={<Users />} />
-      </Routes>
-    </div>
+      <Container sx={{ mt: 3 }}>
+        <Routes>
+          <Route path="products" element={<Products />} />
+          <Route path="users" element={<Users />} />
+        </Routes>
+      </Container>
+    </Box>
   );
 };
 
